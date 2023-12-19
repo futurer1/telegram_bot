d1f864a80e507bec6022d4fdb9b65ac7d1ea5826
Mikhail Afonin <futurer@inbox.ru>
2023-12-19 22:50:30 +0300

    tests

---
+++ mail-service/src/test/java/com/mikhail/telegram/MailControllerTest.java
@@ -0,0 +1,36 @@
+package com.mikhail.telegram;
+
+import com.mikhail.telegram.controller.MailController;
+import com.mikhail.telegram.dto.MailParams;
+import com.mikhail.telegram.service.impl.MailSenderImpl;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.Random;
+
+@ExtendWith(MockitoExtension.class)
+public class MailControllerTest {
+
+    @Mock
+    MailSenderImpl mailSender;
+
+    @InjectMocks
+    MailController controller;
+
+    @Test
+    public void sendActivationMailTest() {
+
+        Random random = new Random();
+        int id = random.nextInt(100);
+
+        MailParams mailParams = new MailParams(
+                String.valueOf(id),
+                "test@mail.ru"
+        );
+
+        controller.sendActivationMail(mailParams);
+    }
+}
